96f89c544ea9600143350ecfd3ef1416af63b74c
Roy Chen <roy97chen@gmail.com>
2024-10-02 17:18:20 +0300

    Note Card

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
     <>
-    <Navbar />
-    
-    </>
-  )
-}
+      <Navbar />
 
-export default Home
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting on 7th August"
+            data="3rd July 2024"
+            content="Meeting with the team to discuss the new project"
+            tags="#meeting #project"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+    </>
+  );
+};
+
+export default Home;
